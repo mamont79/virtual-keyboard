dd62f0242bc28d1f0b314c256ea9262ee5c4b298
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-04-29 22:56:27 +0200

    fix: caps function 29.04.23

--- js/jscript.js
+++ js/jscript.js
@@ -3,9 +3,8 @@ import { winKeys } from "/js/winKeyboard.js";
 import { rusKeys } from "/js/rusKeyboard.js";
 
 
-
-
 let langKeyboard = winKeys;
+
 
 function buildKeyboard() {
   let out = "";
@@ -19,40 +18,40 @@ function buildKeyboard() {
 initKeyboard();
 buildKeyboard();
 
-// bwtbrbrt
+// on press functionality
+
+function capsKeyboard() {
+  document.querySelectorAll(".keyboard .key").forEach(function (element){
+    if(element.getAttribute("data").length == 4) {
+      if (element.textContent.toLowerCase() == element.textContent) {
+        element.textContent = element.textContent.toUpperCase();
+      } else {
+        element.textContent = element.textContent.toLowerCase();
+      }
+    }
+  })
+  
+}
+
+
 
 const capsButton = document.querySelector(".CapsLock");
 
+window.onkeypress = function (event) {
+  if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
+    capsKeyboard();
+  }
+}
+
 window.onkeydown = function (event) {
   if (event.code == "CapsLock") {
-    capsButton.classList.toggle("active");
-    for (let i = 0; i < langKeyboard.length; i++) {
-      if (langKeyboard[i].keyCode <= 90 && langKeyboard[i].keyCode >= 65) {
-        if (langKeyboard[i].key.toLowerCase() == langKeyboard[i].key) {
-          langKeyboard[i].key = langKeyboard[i].key.toUpperCase();
-        } else {
-          langKeyboard[i].key = langKeyboard[i].key.toLowerCase();
-        }
-      } 
-    }
-    buildKeyboard();
-  } else
-   if (event.code != "CapsLock") {
+    document.querySelector(".CapsLock").classList.toggle("active");
+    capsKeyboard()
+  } else if (event.code != "CapsLock") {
     document
     .querySelector('.keyboard .key[data="' + event.code + '"]')
     .classList.add("active");
-    if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
-      for (let i = 0; i < langKeyboard.length; i++) {
-        if (langKeyboard[i].keyCode <= 90 && langKeyboard[i].keyCode >= 65) {
-          langKeyboard[i].key = langKeyboard[i].key.toUpperCase();
-        } else if (langKeyboard[i].key2) {
-          let ch = langKeyboard[i].key;
-          langKeyboard[i].key = langKeyboard[i].key2;
-          langKeyboard[i].key2 = ch;
-        }
-      }
-      buildKeyboard();
-    }
+    
   }
   console.log(event.code)
 };
@@ -62,19 +61,51 @@ window.onkeyup = function (event) {
     document
     .querySelector('.keyboard .key[data="' + event.code + '"]')
     .classList.remove("active");
-    if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
-      for (let i = 0; i < langKeyboard.length; i++) {
-        if (langKeyboard[i].keyCode <= 90 && langKeyboard[i].keyCode >= 65) {
-          langKeyboard[i].key = langKeyboard[i].key.toLowerCase();
-        } else if (langKeyboard[i].key2) {
-          let ch = langKeyboard[i].key;
-          langKeyboard[i].key = langKeyboard[i].key2;
-          langKeyboard[i].key2 = ch;
-        }
-      }
-      buildKeyboard();
-    }
+    // if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
+    //   for (let i = 0; i < langKeyboard.length; i++) {
+    //     if (langKeyboard[i].keyCode <= 90 && langKeyboard[i].keyCode >= 65) {
+    //       langKeyboard[i].key = langKeyboard[i].key.toLowerCase();
+    //     } else if (langKeyboard[i].key2) {
+    //       let ch = langKeyboard[i].key;
+    //       langKeyboard[i].key = langKeyboard[i].key2;
+    //       langKeyboard[i].key2 = ch;
+    //     }
+    //   }
+    //   buildKeyboard();
+    // }
   }
 };
+
+
+// onmousedown functionality
+
+
+document.querySelectorAll('.keyboard .key').forEach(function (element) {
+  element.onmousedown = function(event) {
+    let codeButton = this.getAttribute("data");
+    if (codeButton != "CapsLock") {
+      this.classList.add("active");
+    } else if (codeButton == "CapsLock") {
+      this.classList.toggle("active");
+      capsKeyboard();
+    }
+    console.log(codeButton);
+  }
+});
+
+
+document.querySelectorAll('.keyboard .key').forEach(function (element) {
+  element.onmouseup = function(event) {
+    let codeButton = this.getAttribute("data");
+    if (codeButton != "CapsLock"){
+      this.classList.remove("active");
+    }
+  }
+});
+
+
+
+
+
 
 
