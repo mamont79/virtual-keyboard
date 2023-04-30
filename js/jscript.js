c0caa9c16bb2cfc2f27ce92c99e9a53fd4f9d38e
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-04-30 16:51:23 +0200

    fix: shift function 30.04.23

--- js/jscript.js
+++ js/jscript.js
@@ -1,10 +1,13 @@
 import { initKeyboard, systemOfUser } from "/js/init.js";
 import { winKeys } from "/js/winKeyboard.js";
 import { rusKeys } from "/js/rusKeyboard.js";
-
+import {
+  capsKeyboard,
+  shiftKeyboard,
+  noShiftKeyboard,
+} from "/js/caps-shift.js";
 
 let langKeyboard = winKeys;
-
 
 function buildKeyboard() {
   let out = "";
@@ -20,68 +23,44 @@ buildKeyboard();
 
 // on press functionality
 
-function capsKeyboard() {
-  document.querySelectorAll(".keyboard .key").forEach(function (element){
-    if(element.getAttribute("data").length == 4) {
-      if (element.textContent.toLowerCase() == element.textContent) {
-        element.textContent = element.textContent.toUpperCase();
-      } else {
-        element.textContent = element.textContent.toLowerCase();
-      }
-    }
-  })
-  
-}
-
-
-
 const capsButton = document.querySelector(".CapsLock");
 
 window.onkeypress = function (event) {
   if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
     capsKeyboard();
   }
-}
+};
 
 window.onkeydown = function (event) {
   if (event.code == "CapsLock") {
     document.querySelector(".CapsLock").classList.toggle("active");
-    capsKeyboard()
+    capsKeyboard();
   } else if (event.code != "CapsLock") {
     document
-    .querySelector('.keyboard .key[data="' + event.code + '"]')
-    .classList.add("active");
-    
+      .querySelector('.keyboard .key[data="' + event.code + '"]')
+      .classList.add("active");
   }
-  console.log(event.code)
+  if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
+    shiftKeyboard();
+  }
+  console.log(event.code);
 };
 
 window.onkeyup = function (event) {
   if (event.code != "CapsLock") {
     document
-    .querySelector('.keyboard .key[data="' + event.code + '"]')
-    .classList.remove("active");
-    // if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
-    //   for (let i = 0; i < langKeyboard.length; i++) {
-    //     if (langKeyboard[i].keyCode <= 90 && langKeyboard[i].keyCode >= 65) {
-    //       langKeyboard[i].key = langKeyboard[i].key.toLowerCase();
-    //     } else if (langKeyboard[i].key2) {
-    //       let ch = langKeyboard[i].key;
-    //       langKeyboard[i].key = langKeyboard[i].key2;
-    //       langKeyboard[i].key2 = ch;
-    //     }
-    //   }
-    //   buildKeyboard();
-    // }
+      .querySelector('.keyboard .key[data="' + event.code + '"]')
+      .classList.remove("active");
+    if (event.code == "ShiftRight" || event.code == "ShiftLeft") {
+      noShiftKeyboard();
+    }
   }
 };
 
-
 // onmousedown functionality
 
-
-document.querySelectorAll('.keyboard .key').forEach(function (element) {
-  element.onmousedown = function(event) {
+document.querySelectorAll(".keyboard .key").forEach(function (element) {
+  element.onmousedown = function (event) {
     let codeButton = this.getAttribute("data");
     if (codeButton != "CapsLock") {
       this.classList.add("active");
@@ -89,23 +68,21 @@ document.querySelectorAll('.keyboard .key').forEach(function (element) {
       this.classList.toggle("active");
       capsKeyboard();
     }
+    if (codeButton == "ShiftRight" || codeButton == "ShiftLeft") {
+      shiftKeyboard();
+    }
     console.log(codeButton);
-  }
+  };
 });
 
-
-document.querySelectorAll('.keyboard .key').forEach(function (element) {
-  element.onmouseup = function(event) {
+document.querySelectorAll(".keyboard .key").forEach(function (element) {
+  element.onmouseup = function (event) {
     let codeButton = this.getAttribute("data");
-    if (codeButton != "CapsLock"){
+    if (codeButton != "CapsLock") {
       this.classList.remove("active");
     }
-  }
+    if (codeButton == "ShiftRight" || codeButton == "ShiftLeft") {
+      noShiftKeyboard();
+    }
+  };
 });
-
-
-
-
-
-
-
